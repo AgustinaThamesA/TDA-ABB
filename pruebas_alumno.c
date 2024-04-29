083dcbb1430269a1aece1b35b6bba9d01f26aa15
athames <athames@fi.uba.ar>
2024-04-29 19:35:00 -0300

    Error de memoria en abb_insertar. Puede ser un error del destruir, de la manera de insertar, o mismo de las pruebas.

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,100 @@
 #include "pa2m.h"
+#include "src/abb_estructura_privada.h"
+#include "src/abb.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void prueba_simple()
+typedef struct cosa {
+	int clave;
+	char contenido[10];
+} cosa_t;
+
+cosa_t *crear_cosa(int clave)
 {
-	int i = 14;
-	int j = 18;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	cosa_t *c = (cosa_t *)malloc(sizeof(cosa_t));
+	if (c)
+		c->clave = clave;
+	return c;
 }
+
+int comparador(void *elemento1, void *elemento2){
+	if (elemento1 == NULL || elemento2 == NULL || ((cosa_t*)elemento1)->clave == ((cosa_t*)elemento2)->clave)
+		return 0;
+	if (((cosa_t*)elemento1)->clave > ((cosa_t*)elemento2)->clave)
+		return 1;
+	return -1;
+}
+
+void destruir_elemento(void *elemento){
+	if (elemento == NULL)
+		return;
+	free(elemento);
+}
+
+void crear_arbol_devuelve_arbol_con_nodos_null(){
+	abb_t *abb = abb_crear(NULL);
+	pa2m_afirmar(abb_vacio(abb) && abb_tamanio(abb) == 0, "Se crea árbol vacío y con tamaño 0.");
+	abb_destruir(abb);
+}
+
+void inserta_raiz(){
+	abb_t *abb = NULL;
+	cosa_t *prueba_100 = crear_cosa(100);
+	abb_t *prueba = abb_insertar(abb, prueba_100);
+	pa2m_afirmar(prueba == 0, "No se puede insertar en un árbol nulo.");
+	destruir_elemento(prueba_100);
+
+	abb = abb_crear(NULL);
+
+	cosa_t *prueba_1 = crear_cosa(1);
+	cosa_t *prueba_2 = crear_cosa(2);
+	cosa_t *prueba_3 = crear_cosa(3);
+	cosa_t *prueba_4 = crear_cosa(4);
+	cosa_t *prueba_5 = crear_cosa(5);
+	cosa_t *prueba_6 = crear_cosa(6);
+
+	prueba = abb_insertar(abb, prueba_3);
+
+	pa2m_afirmar(!abb_vacio(abb) && abb_tamanio(abb) == 1, "Inserta correctamente la raíz del árbol.");
+	
+	abb_insertar(abb, prueba_2);
+	abb_insertar(abb, prueba_1);
+	abb_insertar(abb, prueba_6);
+	abb_insertar(abb, prueba_4);
+	prueba = abb_insertar(abb, prueba_5);
+	pa2m_afirmar(!abb_vacio(abb) && abb_tamanio(abb) == 6, "Inserta correctamente 5 elementos a un árbol que ya tiene raíz.");
+
+	destruir_elemento(prueba_1);
+	destruir_elemento(prueba_2);
+	destruir_elemento(prueba_3);
+	destruir_elemento(prueba_4);
+	destruir_elemento(prueba_5);
+	destruir_elemento(prueba_6);
+	abb_destruir(prueba);
+	abb_destruir(abb);
+}
+
 
 int main()
 {
 	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+		"\n======================== Pruebas creación ========================");
+	crear_arbol_devuelve_arbol_con_nodos_null();
+
+	pa2m_nuevo_grupo(
+		"\n======================== Pruebas inserción ========================");
+	inserta_raiz();
+
+
+	pa2m_nuevo_grupo(
+		"\n======================== Pruebas eliminación ========================");
+
+	pa2m_nuevo_grupo(
+		"\n======================== Pruebas recorrer ========================");
+	
+	pa2m_nuevo_grupo(
+		"\n======================== Pruebas destruir todo ========================");
 
 	return pa2m_mostrar_reporte();
 }
--- src/abb.c
+++ src/abb.c
@@ -3,52 +3,330 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+nodo_abb_t *nodo_crear(void *elemento){
+	nodo_abb_t *nodo = calloc(1, sizeof(nodo_abb_t));
+	if (nodo == NULL)
+		return NULL;
+	nodo->elemento = elemento;
+	nodo->izquierda = NULL;
+	nodo->derecha = NULL;
+	return nodo;
+}
+ 
 abb_t *abb_crear(abb_comparador comparador)
 {
-	return NULL;
+	abb_t *abb = calloc(1, sizeof(abb_t));
+	if (abb == NULL)
+		return NULL;
+	abb->comparador = comparador;
+	abb->tamanio = 0;
+	abb->nodo_raiz = NULL;
+	return abb;
 }
+
+nodo_abb_t *agregar_nodo_abb(nodo_abb_t* nodo, void* elemento, abb_comparador comparador){
+	if (nodo == NULL){
+		nodo_abb_t *nodo_aux = nodo_crear(elemento);
+		if (nodo_aux == NULL)
+			return NULL;
+		return nodo_aux;
+	}
+
+	if (comparador(elemento, nodo->elemento) <= 0)
+		nodo->izquierda = agregar_nodo_abb(nodo->izquierda, elemento, comparador);
+	
+	if (comparador(elemento, nodo->elemento) > 0)
+		nodo->derecha = agregar_nodo_abb(nodo->derecha, elemento, comparador);
+	
+	return nodo;
+}
+
 
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
+	if (arbol == NULL)
+		return NULL;
+
+	arbol->nodo_raiz = agregar_nodo_abb(arbol->nodo_raiz, elemento, arbol->comparador);
+	arbol->tamanio++;
+
 	return arbol;
 }
+
+
+/*
+abb_t *abb_insertar(abb_t *arbol, void *elemento){
+	if (arbol == NULL)
+		return NULL;
+
+	if (arbol->nodo_raiz == NULL){
+		arbol->nodo_raiz = nodo_crear(elemento);
+		if (arbol->nodo_raiz == NULL)
+			return NULL;
+		arbol->tamanio = 1;
+		return arbol;
+	}
+
+	if (arbol->comparador(elemento, arbol->nodo_raiz->elemento) > 0){
+		abb_t *arbol_auxiliar1 = abb_crear(arbol->comparador);
+		arbol_auxiliar1->nodo_raiz = arbol->nodo_raiz->derecha;
+		arbol->nodo_raiz->derecha = abb_insertar(arbol_auxiliar1, elemento);
+	} else {
+		abb_t *arbol_auxiliar2 = abb_crear(arbol->comparador);
+		arbol_auxiliar2->nodo_raiz = arbol->nodo_raiz->izquierda;
+		arbol->nodo_raiz->izquierda = abb_insertar(arbol_auxiliar2, elemento);
+	}
+
+	(arbol->tamanio)++;
+	return arbol;
+}
+*/
 
 void *abb_quitar(abb_t *arbol, void *elemento)
 {
 	return elemento;
 }
 
+nodo_abb_t *buscar_nodo_abb(nodo_abb_t *nodo, void *elemento, abb_comparador(comparador)){
+	if (nodo == NULL || nodo->elemento == NULL)
+		return NULL;
+	
+	if (comparador(elemento, nodo->elemento) <= 0)
+		return buscar_nodo_abb(nodo->izquierda, elemento, comparador);
+	
+	if (comparador(elemento, nodo->elemento) > 0)
+		return buscar_nodo_abb(nodo->derecha, elemento, comparador);
+	
+	return nodo;
+}
+
 void *abb_buscar(abb_t *arbol, void *elemento)
 {
-	return elemento;
+	if (abb_vacio(arbol))
+		return NULL;
+	
+	nodo_abb_t *nodo_a_buscar = buscar_nodo_abb(arbol->nodo_raiz, elemento, arbol->comparador);
+	
+	if (nodo_a_buscar == NULL || nodo_a_buscar->elemento == NULL)
+		return NULL;
+	
+	return nodo_a_buscar->elemento;
 }
 
 bool abb_vacio(abb_t *arbol)
 {
-	return true;
+	return (arbol == NULL || arbol->nodo_raiz == NULL);
 }
 
 size_t abb_tamanio(abb_t *arbol)
 {
-	return 0;
+	return (arbol == NULL) ? 0 : arbol->tamanio;
+}
+
+void destruir_nodo_abb(abb_t *arbol, nodo_abb_t *nodo){
+	if (nodo == NULL || arbol == NULL)
+		return;
+	
+	if (nodo->izquierda != NULL)
+		destruir_nodo_abb(arbol, nodo->izquierda);
+	
+	if (nodo->derecha != NULL)
+		destruir_nodo_abb(arbol, nodo->derecha);
+	
+	free(nodo->elemento);
+	free(nodo);
 }
 
 void abb_destruir(abb_t *arbol)
 {
+	if (arbol == NULL)
+		return;
+	if (arbol->nodo_raiz)
+		destruir_nodo_abb(arbol, arbol->nodo_raiz);
+	free(arbol);
+}
+
+void destruir_todo_nodo_abb(abb_t *arbol, nodo_abb_t *nodo, void (*destructor)(void *)){
+	if (nodo == NULL || arbol == NULL)
+		return;
+	
+	if (nodo->izquierda != NULL)
+		destruir_todo_nodo_abb(arbol, nodo->izquierda, destructor);
+	
+	if (nodo->derecha != NULL)
+		destruir_todo_nodo_abb(arbol, nodo->derecha, destructor);
+	
+	if (destructor)
+		destructor(nodo->elemento);
+	
+	free(nodo->elemento);
+	free(nodo);
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 {
+	if (arbol == NULL)
+		return;
+	if (arbol->nodo_raiz)
+		destruir_todo_nodo_abb(arbol, arbol->nodo_raiz, destructor);
+	free(arbol);
+}
+
+size_t recorrido_inorden_con_cada_elem(nodo_abb_t *raiz, bool (*funcion)(void *, void *), size_t cantidad, void *aux, bool *recorrido_completo){
+	if (raiz == NULL || (*recorrido_completo)) 
+		return cantidad;
+	
+	if (raiz->izquierda)
+		cantidad = recorrido_inorden_con_cada_elem(raiz->izquierda, funcion, cantidad, aux, recorrido_completo);
+	
+	if ((*recorrido_completo))
+		return cantidad;
+	
+	(*recorrido_completo) = funcion(raiz->elemento, aux);
+	cantidad++;
+
+	if ((*recorrido_completo))
+		return cantidad;
+	
+	if (raiz->derecha)
+		cantidad = recorrido_inorden_con_cada_elem(raiz->derecha, funcion, cantidad, aux, recorrido_completo);
+
+	return cantidad;
+}
+
+size_t recorrido_preorden_con_cada_elem(nodo_abb_t *raiz, bool (*funcion)(void *, void *), size_t cantidad, void *aux, bool *recorrido_completo){
+	if (raiz == NULL || (*recorrido_completo)) 
+		return cantidad;
+
+	(*recorrido_completo) = funcion(raiz->elemento, aux);
+	cantidad++;
+
+	if ((*recorrido_completo))
+		return cantidad;
+	
+	if (raiz->izquierda)
+		cantidad = recorrido_preorden_con_cada_elem(raiz->izquierda, funcion, cantidad, aux, recorrido_completo);
+	
+	if ((*recorrido_completo))
+		return cantidad;
+	
+	if (raiz->derecha)
+		cantidad = recorrido_preorden_con_cada_elem(raiz->derecha, funcion, cantidad, aux, recorrido_completo);
+
+	return cantidad;
+}
+
+size_t recorrido_postorden_con_cada_elem(nodo_abb_t *raiz, bool (*funcion)(void *, void *), size_t cantidad, void *aux, bool *recorrido_completo){
+	if (raiz == NULL || (*recorrido_completo)) 
+		return cantidad;
+	
+	if (raiz->izquierda)
+		cantidad = recorrido_postorden_con_cada_elem(raiz->izquierda, funcion, cantidad, aux, recorrido_completo);
+	
+	if ((*recorrido_completo))
+		return cantidad;
+	
+	if (raiz->derecha)
+		cantidad = recorrido_postorden_con_cada_elem(raiz->derecha, funcion, cantidad, aux, recorrido_completo);
+
+	(*recorrido_completo) = funcion(raiz->elemento, aux);
+	cantidad++;
+
+	if ((*recorrido_completo))
+		return cantidad;
+
+	return cantidad;
 }
 
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
 			     bool (*funcion)(void *, void *), void *aux)
 {
-	return 0;
+	if (arbol == NULL || arbol->nodo_raiz == NULL || funcion == NULL)
+		return 0;
+	
+	size_t cant_veces_funcion = 0;
+	bool *recorrido_completo = NULL;
+
+	if (recorrido == INORDEN){
+		recorrido_inorden_con_cada_elem(arbol->nodo_raiz, funcion, cant_veces_funcion, aux, recorrido_completo);
+	} else if (recorrido == PREORDEN){
+		recorrido_preorden_con_cada_elem(arbol->nodo_raiz, funcion, cant_veces_funcion, aux, recorrido_completo);
+	} else if (recorrido == POSTORDEN){
+		recorrido_postorden_con_cada_elem(arbol->nodo_raiz, funcion, cant_veces_funcion, aux, recorrido_completo);
+	} 
+	
+	return cant_veces_funcion;
+}
+
+bool recorrido_inorden(nodo_abb_t *nodo, void **array, size_t tamanio_array, size_t *cant_almacenados){
+	if (nodo == NULL)
+		return true;
+
+	bool hay_mas_elementos = recorrido_inorden(nodo->izquierda, array, tamanio_array, cant_almacenados);
+
+	if(hay_mas_elementos == false)
+		return false;
+
+	if((*cant_almacenados) >= tamanio_array)
+		return true;
+	
+	array[*cant_almacenados] = nodo->elemento;
+	(*cant_almacenados)++;
+
+	return recorrido_inorden(nodo->derecha, array, tamanio_array, cant_almacenados);
+}
+
+bool recorrido_preorden(nodo_abb_t *nodo, void **array, size_t tamanio_array, size_t *cant_almacenados){
+	if (nodo == NULL || (*cant_almacenados) >= tamanio_array)
+		return true;
+	
+	array[*cant_almacenados] = nodo->elemento;
+	(*cant_almacenados)++;
+
+	bool hay_mas_elementos = recorrido_preorden(nodo->izquierda, array, tamanio_array, cant_almacenados);
+
+	if(hay_mas_elementos == false)
+		return false;
+	
+	return recorrido_preorden(nodo->derecha, array, tamanio_array, cant_almacenados);
+}
+
+bool recorrido_postorden(nodo_abb_t *nodo, void **array, size_t tamanio_array, size_t *cant_almacenados){
+	if (nodo == NULL)
+		return true;
+
+	bool hay_mas_elementos = recorrido_postorden(nodo->izquierda, array, tamanio_array, cant_almacenados);
+
+	if(hay_mas_elementos == false)
+		return false;
+
+	if (recorrido_postorden(nodo->derecha, array, tamanio_array, cant_almacenados) == false)
+		return false;
+
+	if((*cant_almacenados) >= tamanio_array)
+		return true;
+	
+	array[*cant_almacenados] = nodo->elemento;
+	(*cant_almacenados)++;
+
+	return true;
 }
 
 size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array,
 		    size_t tamanio_array)
 {
-	return 0;
+	if (arbol == NULL || tamanio_array == 0)
+		return 0;
+
+	size_t cant_almacenados = 0;
+
+	if (recorrido == INORDEN){
+		recorrido_inorden(arbol->nodo_raiz, array, tamanio_array, &cant_almacenados);
+	} else if (recorrido == PREORDEN){
+		recorrido_preorden(arbol->nodo_raiz, array, tamanio_array, &cant_almacenados);
+	} else if (recorrido == POSTORDEN){
+		recorrido_postorden(arbol->nodo_raiz, array, tamanio_array, &cant_almacenados);
+	} 
+	
+	return cant_almacenados;
 }
